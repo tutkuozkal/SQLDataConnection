39400bee1d891e2d18f1bf2a8b595929272cad2e
Tutku ÖZKAL <tutkularda@hotmail.com>
2021-01-30 16:34:30 +0400

    Add project files.

---
+++ Connector1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLDataConnection
+{
+    class Connector
+    {
+        public void Perform(string query)
+        {
+            Console.WriteLine("Sorgu Olusturuldu.");
+        }
+    }
+}
